600f59ba3767f4ea8094085dca371f310b7307ca
Christian Jay A Bernardo <christianjayapanbernardo@gmail.com>
2026-03-16 10:13:49 +0800

    fix

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -12,7 +12,7 @@ echo "Caching views..."
 php artisan view:cache
 
 echo "Running migrations..."
-php artisan migrate --force
+php artisan migrate:fresh --force
 
 echo "Running seeders..."  # ADD THIS
 php artisan db:seed --force  # ADD THIS
